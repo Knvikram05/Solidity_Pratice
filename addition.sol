2a46fa8bdb62252839aaae7f25d3c44a2e564692
Vikram <142532704+Knvikram05@users.noreply.github.com>
2024-02-26 21:37:59 +0530

    Update addition.sol

--- addition.sol
+++ addition.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+contract addition{
 
+int a = 50;
+int b = 50;
+
+function add()public view  returns(int){
+    int result = a+b;
+     return result ;
+
+}
+
+}
+
+//Output
+
+//a,b= 5,7
+//result =12
